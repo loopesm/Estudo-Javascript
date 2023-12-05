734d49e627963e61c13854be361912b8c149a2c4
Moises Lopes <moiseslopes999@gmail.com>
2023-12-05 19:57:03 -0300

    Upload Arquivos

---
+++ js/variaveis.js
@@ -0,0 +1,20 @@
+// variavel tipo string
+var texto = "Curso de JavaScript"
+
+// variavel tipo number
+
+var numeroInt = -7
+var numeroFrac = 1.2
+
+// variavel tipo boolean
+
+var teste1 = true
+var teste2 = false
+
+document.write(texto,"<br>")
+document.write(numeroInt,"<br>")
+document.write(numeroFrac,"<br>")
+document.write(teste1,"<br>")
+document.write(teste2,"<br>")
+
+
